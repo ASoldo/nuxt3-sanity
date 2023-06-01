f9a888ae13f8843f3ae2b294dec894710a36174f
ASoldo <soldo.andrej@gmail.com>
2023-06-02 00:10:22 +0200

    Update packages
    Update Sanity to @latest.
    Small clean up.

--- supabase/functions/edge-lord/index.ts
+++ supabase/functions/edge-lord/index.ts
@@ -2,13 +2,14 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 console.log("Hello from Functions!");
 
 serve(async (req: any) => {
   const corsHeaders = {
-    "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
+    "Access-Control-Allow-Headers":
+      "authorization, x-client-info, apikey, content-type",
     "Access-Control-Allow-Origin": "*",
   };
   if (req.method === "OPTIONS") {
@@ -31,19 +32,6 @@ serve(async (req: any) => {
       status: 400,
     });
   }
-
-  // const data = {
-  //   message: `Hello I am edge function!`,
-  // };
-  //
-  // return new Response(JSON.stringify(data), {
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
-  //     "Access-Control-Allow-Origin": "*",
-  //   },
-  // });
-});
 
 // To invoke:
 // curl -i --location --request POST 'http://localhost:54321/functions/v1/' --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' --header 'Content-Type: application/json' --data '{"name":"Functions"}'
