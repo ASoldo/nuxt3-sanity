f9139f0b5516a16738798c244c96980435516551
ASoldo <soldo.andrej@gmail.com>
2023-06-02 00:24:51 +0200

    Toasty!
    Small syntax fix

--- supabase/functions/edge-lord/index.ts
+++ supabase/functions/edge-lord/index.ts
@@ -21,7 +21,7 @@ serve(async (req: any) => {
     const data = {
       message: `olla ${name}`,
     };
-    console.log("Olla");
+    console.log(`Olla ${name}!`);
     return new Response(JSON.stringify(data), {
       headers: { ...corsHeaders, "Content-Type": "application/json" },
       status: 200,
@@ -32,6 +32,4 @@ serve(async (req: any) => {
       status: 400,
     });
   }
-
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' --header 'Content-Type: application/json' --data '{"name":"Functions"}'
+});
