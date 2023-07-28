b3cab1c772d82b6b61c22468b682409b330ba469
ASoldo <soldo.andrej@gmail.com>
2023-07-28 19:35:06 +0200

    Update

--- server/api/start_session.ts
+++ server/api/start_session.ts
@@ -22,10 +22,12 @@ export default defineEventHandler(async (event) => {
   };
 
   const supabaseUrl =
-    (import.meta as any).env.SUPABASE_URL + "/rest/v1/game_sessions";
+    "https://kxbzixfkcjexfwfacnzq.supabase.co/rest/v1/game_sessions";
   const supabaseHeaders = {
-    apikey: import.meta.env.API_KEY,
-    Authorization: `Bearer ${import.meta.env.API_KEY}`,
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4YnppeGZrY2pleGZ3ZmFjbnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ5NTQzODEsImV4cCI6MjAwMDUzMDM4MX0.Jl5GMoQSyVVgOFAHRIyCEFFgsGe1YahNVCaCjehO0hw",
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4YnppeGZrY2pleGZ3ZmFjbnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ5NTQzODEsImV4cCI6MjAwMDUzMDM4MX0.Jl5GMoQSyVVgOFAHRIyCEFFgsGe1YahNVCaCjehO0hw`,
+
     "Content-Type": "application/json",
     Prefer: "return=representation",
   };
