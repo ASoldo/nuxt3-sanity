d07039a52f611a5cba351d528d3bebf9a2400040
ASoldo <soldo.andrej@gmail.com>
2024-01-08 18:39:16 +0100

    Idk

--- pages/index.vue
+++ pages/index.vue
@@ -153,19 +153,19 @@ const {
 } = useCookieControl();
 
 // example: react to a cookie being accepted
-watch(
-  () => cookiesEnabledIds.value,
-  (current, previous) => {
-    if (
-      !previous?.includes("google-analytics") &&
-      current?.includes("google-analytics")
-    ) {
-      // cookie with id `google-analytics` got added
-      window.location.reload(); // placeholder for your custom change handler
-    }
-  },
-  { deep: true },
-);
+// watch(
+//   () => cookiesEnabledIds.value,
+//   (current, previous) => {
+//     if (
+//       !previous?.includes("google-analytics") &&
+//       current?.includes("google-analytics")
+//     ) {
+//       // cookie with id `google-analytics` got added
+//       window.location.reload(); // placeholder for your custom change handler
+//     }
+//   },
+//   { deep: true },
+// );
 </script>
 
 <style scoped>
