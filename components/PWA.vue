43ffa4a3d06b44a7bc1c963d7775439c0e4ded71
ASoldo <soldo.andrej@gmail.com>
2024-01-08 13:51:00 +0100

    Update

--- components/PWA.vue
+++ components/PWA.vue
@@ -60,16 +60,16 @@ onMounted(() => {
 <template>
   <ClientOnly>
     <!-- You can use $pwa directly in templates! -->
-    <div v-show="$pwa.needRefresh"
-      class="bg-red-100 border-t-4 border-red-500 rounded-b text-blue-900 px-4 py-3 shadow-md sticky bottom-0 z-50">
-      <span> New content available, click on reload button to update. </span>
-
-      <button class="font-bold text-blue-500 hover:text-blue-800 ml-4" @click="$pwa.updateServiceWorker()">
-        Reload now
-      </button>
-    </div>
+    <!-- <div v-show="$pwa.needRefresh" -->
+    <!--   class="bg-red-100 border-t-4 border-red-500 rounded-b text-blue-900 px-4 py-3 shadow-md sticky bottom-0 z-50"> -->
+    <!--   <span> New content available, click on reload button to update. </span> -->
+    <!---->
+    <!--   <button class="font-bold text-blue-500 hover:text-blue-800 ml-4" @click="$pwa.updateServiceWorker()"> -->
+    <!--     Reload now -->
+    <!--   </button> -->
+    <!-- </div> -->
     <div v-if="$pwa?.offlineReady || $pwa?.needRefresh"
-      class="bg-red-100 border-t-4 border-red-500 rounded-b text-blue-900 px-4 py-3 shadow-md sticky bottom-0 z-50"
+      class="bg-blue-100 border-t-4 border-red-500 rounded-b text-blue-900 px-4 py-3 shadow-md sticky bottom-0 z-50"
       role="alert">
       <div class="flex">
         <div class="py-1">
