e61595f4a5270d29f1f1e162a10aee1f39ac508b
ASoldo <soldo.andrej@gmail.com>
2024-01-08 20:19:07 +0100

    Udate

--- pages/index.vue
+++ pages/index.vue
@@ -14,10 +14,6 @@
         <div class="flex flex-row z-10 overflow-hidden">
           <img src="../assets/images/medalja.png" alt="" class="h-24" />
           <img src="../assets/images/rang_list.png" alt="" class="h-32" />
-          <!-- <h1 class="font-kaufland-bold text-2xl md:text-4xl text-white"> -->
-          <!--   K-MARKE(t) <br /> -->
-          <!--   RANG LISTA -->
-          <!-- </h1> -->
         </div>
         <h1 class="text-white font-kaufland-bold z-10">
           TOP 10 najboljih igrača danas
@@ -26,7 +22,6 @@
           <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg">
             <table class="min-w-full">
               <tbody class="">
-                <!-- :class="{ 'bg-black': item.user_uuid == user?.id }" -->
                 <tr v-for="(item, index) in leaderboard?.data" :key="index">
                   <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
                     <span class="text-white text-center">
@@ -34,11 +29,9 @@
                     </span>
                   </td>
                   <td class="text-center text-white px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium">
-                    <!-- <span class="pi pi-user pr-2 text-blue-500"></span> -->
                     {{ item.profiles.first_name }}
                   </td>
                   <td class="text-center px-6 py-4 whitespace-no-wrap text-sm leading-5 text-white">
-                    <!-- <span class="pi pi-check px-1 text-green-500"></span> -->
                     &nbsp;{{ sliceScore(item.best_score) }}
                   </td>
                 </tr>
